1c4a2991634004212e36ee6a8f6f33a77899bc9a
Ariel De Ocampo <arielmakestuff@gmail.com>
2017-02-05 20:56:55 -0500

    Initial implementation of example fibonacci calculator

---
+++ src/main.rs
@@ -0,0 +1,75 @@
+// -*- coding: utf-8 -*-
+// src/tmp.rs
+// Copyright (C) 2017 authors and contributors (see AUTHORS file)
+//
+// This file is released under the MIT License.
+
+// ===========================================================================
+// Imports
+// ===========================================================================
+
+// Externs
+
+// Stdlib imports
+use std::collections::HashMap;
+use std::io;
+
+// Third-party imports
+
+// Local imports
+
+
+// ===========================================================================
+//
+// ===========================================================================
+
+
+fn fib(cache: &mut HashMap<usize, usize>, i: usize) -> usize {
+    // Compute val
+    match cache.get(&i) {
+        Some(&val) => val,
+        None if i <= 3 => {
+            cache.insert(i, i);
+            i
+        },
+        None => {
+            let ret = fib(cache, i-2) + fib(cache, i-1);
+            cache.insert(i, ret);
+            ret
+        },
+    }
+}
+
+fn main() {
+    println!("Fibonacci calculator");
+    println!("n:");
+
+    let mut store = HashMap::new();
+
+    loop {
+        // Read input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+
+        // Exit if "q" received
+        if input.trim() == "q" {
+            break;
+        }
+
+        // Convert input to usize
+        let n: usize = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let ret: usize = fib(&mut store, n);
+        println!("Fib({}) = {}", n, ret);
+    }
+
+}
+
+
+// ===========================================================================
+//
+// ===========================================================================
